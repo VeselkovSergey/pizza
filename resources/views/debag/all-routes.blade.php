1d939f77e685d46961c62ec3cb21a3b546d04d42
Sergey <veselok@gmail.com>
2021-09-30 00:32:08 +0300

    -

--- resources/views/debag/all-routes.blade.php
+++ resources/views/debag/all-routes.blade.php
@@ -2,12 +2,12 @@
 
 @section('content')
     <div class="flex-column">
-        <a href="{{route('modification-type-create')}}">1 Новый тип модификации</a>
-        <a href="{{route('modification-create')}}">2 Новая модификация</a>
-        <a href="{{route('ingredients-create')}}">3 Новый ингредиент</a>
-        <a href="{{route('supplier-create')}}">4 Поставщики</a>
-        <a href="{{route('supply-create')}}">5 Новая поставка</a>
-        <a href="{{route('product-create')}}">6 Новый продукт</a>
+        <a href="{{route('modification-type-create')}}">1 Новый тип модификации (размер/толщина теста)</a>
+        <a href="{{route('modification-create')}}">2 Новая модификация (сама модификация со значением: Размер: 25см)</a>
+        <a href="{{route('ingredients-create')}}">3 Новый ингредиент (для тех карты + продукт кока-кола состоит из ингредиента кока-кола)</a>
+        <a href="{{route('supplier-create')}}">4 Поставщики (метро/рынок/рога и копыта)</a>
+        <a href="{{route('supply-create')}}">5 Новая поставка (поставка. тут вносим ингредиенты)</a>
+        <a href="{{route('product-create')}}">6 Новый продукт (хз доделал или нет)))))</a>
     </div>
 @stop
 
