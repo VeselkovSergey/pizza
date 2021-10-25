12ec0d9c2bcaba139930855f6e2e977d571c78cc
Sergey <veselok@gmail.com>
2021-10-25 15:59:54 +0300

    +

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Services\Telegram\Telegram;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -26,13 +27,13 @@ Route::view('all-routes', 'debag.all-routes');
 
 Route::get('resources/{directory}/{fileName}', [Controllers\Resources\ResourceController::class, 'GetResources']);
 
-Route::group(['prefix' => 'catalog'], function() {
+Route::group(['prefix' => 'catalog'], function () {
 
     Route::get('/', [Controllers\Catalog\CatalogController::class, 'Index'])->name('catalog');
 
 });
 
-Route::group(['prefix' => 'products'], function() {
+Route::group(['prefix' => 'products'], function () {
 
     Route::get('/all-admin', [Controllers\Products\ProductsController::class, 'IndexAdmin'])->name('all-products-admin-page');
     Route::get('/all-products', [Controllers\Products\ProductsController::class, 'GetAllProducts'])->name('all-products');
@@ -41,7 +42,7 @@ Route::group(['prefix' => 'products'], function() {
 
 });
 
-Route::group(['prefix' => 'modifications'], function() {
+Route::group(['prefix' => 'modifications'], function () {
 
     Route::get('/create', [Controllers\Modifications\ModificationsController::class, 'Create'])->name('modification-create');
     Route::post('/save', [Controllers\Modifications\ModificationsController::class, 'Save'])->name('modification-save');
@@ -49,14 +50,14 @@ Route::group(['prefix' => 'modifications'], function() {
 
 });
 
-Route::group(['prefix' => 'types-modifications'], function() {
+Route::group(['prefix' => 'types-modifications'], function () {
 
     Route::get('/create', [Controllers\TypesModifications\TypesModificationsController::class, 'Create'])->name('modification-type-create');
     Route::post('/save', [Controllers\TypesModifications\TypesModificationsController::class, 'Save'])->name('modification-type-save');
 
 });
 
-Route::group(['prefix' => 'ingredients'], function() {
+Route::group(['prefix' => 'ingredients'], function () {
 
     Route::get('/create', [Controllers\Ingredients\IngredientsController::class, 'Create'])->name('ingredients-create');
     Route::post('/save', [Controllers\Ingredients\IngredientsController::class, 'Save'])->name('ingredients-save');
@@ -64,27 +65,27 @@ Route::group(['prefix' => 'ingredients'], function() {
 
 });
 
-Route::group(['prefix' => 'supply'], function() {
+Route::group(['prefix' => 'supply'], function () {
 
     Route::get('/create', [Controllers\Supply\SupplyController::class, 'Create'])->name('supply-create');
     Route::post('/save', [Controllers\Supply\SupplyController::class, 'Save'])->name('supply-save');
 
 });
 
-Route::group(['prefix' => 'suppliers'], function() {
+Route::group(['prefix' => 'suppliers'], function () {
 
     Route::get('/create', [Controllers\Suppliers\SuppliersController::class, 'Create'])->name('supplier-create');
     Route::post('/save', [Controllers\Suppliers\SuppliersController::class, 'Save'])->name('supplier-save');
 
 });
 
-Route::group(['prefix' => 'order'], function() {
+Route::group(['prefix' => 'order'], function () {
 
     Route::post('/create', [Controllers\Orders\OrdersController::class, 'Create'])->name('order-create');
 
 });
 
-Route::group(['prefix' => 'auth'], function() {
+Route::group(['prefix' => 'auth'], function () {
 
     Route::post('/phone-validation', [Controllers\Auth\AuthController::class, 'PhoneValidation'])->name('phone-validation');
     Route::post('/check-confirmation-code', [Controllers\Auth\AuthController::class, 'CheckConfirmationCode'])->name('check-confirmation-code');
@@ -93,16 +94,16 @@ Route::group(['prefix' => 'auth'], function() {
 
 });
 
-Route::group(['prefix' => 'arm'], function() {
+Route::group(['prefix' => 'arm'], function () {
 
 
-    Route::group(['prefix' => 'administration'], function() {
+    Route::group(['prefix' => 'administration'], function () {
 
         Route::get('/', [Controllers\ARM\AdministratorARMController::class, 'Index'])->name('administrator-arm-page');
 
     });
 
-    Route::group(['prefix' => 'management'], function() {
+    Route::group(['prefix' => 'management'], function () {
 
         Route::get('/', [Controllers\ARM\ManagerARMController::class, 'Index'])->name('manager-arm-page');
 
@@ -123,7 +124,7 @@ Route::group(['prefix' => 'arm'], function() {
 
     });
 
-    Route::group(['prefix' => 'chef'], function() {
+    Route::group(['prefix' => 'chef'], function () {
 
         Route::get('/', [Controllers\ARM\ChefARMController::class, 'Index'])->name('chef-arm-page');
 
@@ -146,7 +147,7 @@ Route::group(['prefix' => 'arm'], function() {
  *  test routes
  */
 Route::get('/test-ucaller', function () {
-    return ;
+    return;
 //    $ucaller = new App\Services\Ucaller\Ucaller();
 //    $balance = $ucaller->GetBalance();
 //    $info = $ucaller->GetInfo();
@@ -164,42 +165,100 @@ Route::get('/test-bot', function () {
 
 Route::get('/test-parse', function () {
 
+    $adresses = [
+        'Дубна, Московская область, Россия, улица Вернова, 9',
+        'улица Попова, 3, Дубна, Московская область, Россия',
+        'улица Понтекорво, 2, Дубна, Московская область, Россия',
+        'Дубна, Московская область, Россия, улица Вернова, 9',
+    ];
+
+    dd(implode(' ~ ', $adresses));
+
+//    $cookiesRaw = \App\Helpers\ArrayHelper::ObjectToArray(json_decode(file_get_contents('./cookie.json')));
+//    $cookies = '';
+//    if ($cookiesRaw !== null) {
+//        foreach ($cookiesRaw as $key => $cookie) {
+//            $lastSymbol = array_key_last($cookiesRaw) === $key ? '' : ';';
+//            $cookies .= $key . '=' . $cookie . $lastSymbol;
+//        }
+//    }
+
     $param = http_build_query([
+        'll' => '37.15875'.rand(6, 8).',56.73777'.rand(6, 8).'',
+        'mode' => 'routes',
         'rtext' => 'Дубна, Московская область, Россия, улица Вернова, 9  ~ улица Попова, 3, Дубна, Московская область, Россия ~ улица Понтекорво, 2, Дубна, Московская область, Россия ~ Дубна, Московская область, Россия, улица Вернова, 9',
         'rtt' => 'auto',
+        'z' => ''.rand(10, 20),
     ]);
 
-    $url = 'https://yandex.ru/maps/?' . $param;
+//    $url = 'https://yandex.ru/maps/?' . $param;
+    $url = 'https://yandex.ru/maps/215/dubna/?' . $param;
 
     $headers = [
-        'Host' => '<calculated when request is sent>',
-        'User-Agent' => 'PostmanRuntime/7.28.4',
+        'accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'accept-encoding' => 'gzip, deflate, br',
+        'accept-language' => 'ru',
+        'cache-control' => 'no-cache',
+        'pragma' => 'no-cache',
+        'preferanonymous' => '1',
+        'sec-ch-ua' => '"Chromium";v="94", "Microsoft Edge";v="94", ";Not A Brand";v="99"',
+        'sec-ch-ua-mobile' => '?0',
+        'sec-ch-ua-platform' => '"Windows"',
+        'sec-fetch-dest' => 'document',
+        'sec-fetch-mode' => 'navigate',
+        'sec-fetch-site' => 'none',
+        'sec-fetch-user' => '?1',
+        'upgrade-insecure-requests' => '1',
+        'user-agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36 Edg/94.0.992.50',
+        'viewport-width' => '2560',
+//        'cookie' => $cookies,
+//        'postman-token' => Hash::make(\Illuminate\Support\Str::random()),
     ]; // создаем заголовки
 
     $curl = curl_init(); // создаем экземпляр curl
 
+//    curl_setopt($curl, CURLOPT_HEADER, 1);
     curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($curl, CURLOPT_VERBOSE, 1);
     curl_setopt($curl, CURLOPT_POST, false); //
     curl_setopt($curl, CURLOPT_URL, $url);
 
+    curl_setopt($curl, CURLOPT_AUTOREFERER, true);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1);
+    curl_setopt($curl, CURLOPT_VERBOSE, 1);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 1);
+
     $result = curl_exec($curl);
-    $server_error = curl_error($curl);
 
+//    preg_match_all('/^Set-Cookie:\s*([^;]*)/mi', $result, $matches);
+//    $cookies = [];
+//    foreach($matches[1] as $item) {
+//        parse_str($item, $cookie);
+//        $cookies = array_merge($cookies, $cookie);
+//    }
+//
+//    file_put_contents('./cookie.json', json_encode($cookies));
 
-    dd($result, $server_error);
-    $result = file_get_contents($url);
-    //print_r();
-//    dd($result);
     preg_match('({"appVersion.+})', $result, $matches);
-    dd($matches);
-    $resultYa = json_decode($matches[0]);
+    if (!sizeof($matches)) {
+        $result = str_replace('href="/captcha', 'href="https://yandex.ru/captcha', $result);
+        $result = str_replace('action="/checkcaptcha', 'action="https://yandex.ru/checkcaptcha', $result);
+        $result = str_replace('src="/captcha', 'src="https://yandex.ru/captcha', $result);
+        echo $result;
+        exit;
+    } else {
+        $resultYa = json_decode($matches[0]);
+        $distance = (float)(str_replace(',', '.', $resultYa->routerResponse->routes[0]->distance->text));
+        $startTime = $resultYa->routerResponse->routes[0]->paths[array_key_first($resultYa->routerResponse->routes[0]->paths)]->beginTime->value;
+        $endTime = $resultYa->routerResponse->routes[0]->paths[array_key_last($resultYa->routerResponse->routes[0]->paths)]->endTime->value;
+        $deliveryTime = ($endTime - $startTime) / 60;
 
-    $distance = $resultYa->routerResponse->routes[0]->distance->text;
-    dd($distance, $resultYa);
-    dd();
-    //
+        $data = (object)[
+            'distance' => $distance,
+            'deliveryTime' => $deliveryTime,
+        ];
+        dd($data, $resultYa);
+    }
 });
 
 Route::view('test-maps', 'debag.test');
