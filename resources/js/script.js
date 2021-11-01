df9c9bd5cb93f116028ed5f029efa143b7b62ff2
Sergey <veselok@gmail.com>
2021-11-01 19:08:57 +0300

    +

--- resources/js/script.js
+++ resources/js/script.js
@@ -173,13 +173,17 @@ function ModalWindow(content, closingCallback, flash) {
     document.body.append(modalWindowComponentContainer);
 
     if (typeof content !== 'string') {
-        CloseByScroll(modalWindowComponentContainer, modalWindowContainer, content);
+        CloseByScroll(modalWindowComponentContainer, modalWindowContainer, content, () => {
+            closingCallback ? closingCallback() : '';
+            modalWindowComponentContainer.slowRemove();
+            !flash ? documentBody.classList.remove('scroll-off') : '';
+        });
     }
 
     return modalWindowComponentContainer;
 }
 
-function CloseByScroll(modalWindowComponentContainer, container, content) {
+function CloseByScroll(modalWindowComponentContainer, container, content, closingCallback) {
     let widthClientScreen = document.documentElement.clientWidth;
     if (widthClientScreen < 768) {
 
@@ -217,7 +221,7 @@ function CloseByScroll(modalWindowComponentContainer, container, content) {
                 containerModalWindow.style.transform = 'translateY(0px)';
             } else {
                 containerModalWindow.style.transform = 'translateY(' + heightClientScreen + 'px)';
-                modalWindowComponentContainer.slowRemove();
+                closingCallback ? closingCallback() : '';
             }
         });
     }
