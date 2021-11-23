38871647cfc88e08c8adcba88015bbf9845ea78c
Sergey <veselok@gmail.com>
2021-11-23 21:28:28 +0300

    +

--- resources/views/catalog/index.blade.php
+++ resources/views/catalog/index.blade.php
@@ -196,7 +196,7 @@
             return containerAllModifications;
         }
 
-        let allProducts = {{!! json_decode($allProducts) !!}};
+        let allProducts = {{!! json_encode($allProducts) !!}};
 
     </script>
 
