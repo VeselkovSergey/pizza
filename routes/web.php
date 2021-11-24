6e231c2f679cca94714ff9f16a89cb1991c16183
Sergey <veselok@gmail.com>
2021-11-24 21:02:30 +0300

    пересчет кол-ва ингридиентов

--- routes/web.php
+++ routes/web.php
@@ -267,7 +267,7 @@ Route::view('test-maps', 'debag.test');
 Route::get('/rebase-ing', function () {
     $allIng = \App\Models\ProductModificationsIngredients::all();
     foreach ($allIng as $ing) {
-        if ($ing->ingredient_amount > 0) {
+        if (!is_float($ing->ingredient_amount)) {
             $ing->ingredient_amount = ($ing->ingredient_amount * 1000);
             $ing->save();
         }
