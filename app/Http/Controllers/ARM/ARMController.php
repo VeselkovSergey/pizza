91e2beeadb0bcbc353924f6a2c9c146f0aa7f513
Sergey Veselkov <veselkov@action-media.ru>
2021-12-09 19:01:07 +0300

    контроллер для всен рутов

---
+++ app/Http/Controllers/ARM/ARMController.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace App\Http\Controllers\ARM;
+
+
+class ARMController
+{
+    public function AllRoutes()
+    {
+        return view('debag.all-routes');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ use App\Http\Controllers;
 
 Route::get('/', [Controllers\Catalog\CatalogController::class, 'Index'])->name('home-page');
 
-Route::view('all-routes', 'debag.all-routes')->name('all-routes');
+Route::get('all-routes', [Controllers\ARM\ARMController::class, 'AllRoutes'])->name('all-routes');
 
 Route::get('resources/{directory}/{fileName}', [Controllers\Resources\ResourceController::class, 'GetResources']);
 
