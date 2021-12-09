3f2c16f9098181160307f1e771571ee5a114c860
Sergey Veselkov <veselkov@action-media.ru>
2021-12-09 14:19:58 +0300

    BOT

--- routes/web.php
+++ routes/web.php
@@ -145,6 +145,7 @@ Route::group(['prefix' => 'arm'], function () {
 Route::get('telegram-bot-webhook', [Controllers\TelegramBOT\TelegramBOT::class, 'Index'])->name('telegram-bot-webhook');
 
 
+
 /*
  *  test routes
  */
