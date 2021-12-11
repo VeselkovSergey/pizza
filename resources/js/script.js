e5168dcb18ee4d3eb3bcdb6cc7093149b49e6b5d
Sergey <veselok@gmail.com>
2021-12-11 20:12:11 +0300

    loader

--- resources/js/script.js
+++ resources/js/script.js
@@ -3,6 +3,17 @@ const SvgPlusButton = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height
 const SvgMinusButton = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16"> <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8z"/> </svg>';
 const SvgTrashButton = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16"> <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/> <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/> </svg>';
 
+function LoaderShow() {
+    let loader = document.createElement("div");
+    loader.className = 'loader';
+    loader.innerHTML = '<div class="lds-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>';
+    document.body.prepend(loader);
+}
+
+function LoaderHide() {
+    document.body.querySelector('.loader').remove();
+}
+
 function Ajax(url, method, formDataRAW) {
     return new Promise(function (resolve, reject) {
         let formData = new FormData();
@@ -558,7 +569,9 @@ function CreateOrder(orderId) {
         orderId: orderId,
     };
 
+    LoaderShow();
     Ajax(routeOrderCreate, 'POST', data).then((response) => {
+        LoaderHide();
         FlashMessage(response.message);
         if (response.status === true) {
             basketWindow.slowRemove();
