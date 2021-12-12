8d658e63814c8154b9de9c00799148b311a1f47f
Sergey <veselok@gmail.com>
2021-12-12 19:09:08 +0300

    Пользователь, который заказал - админ

--- database/migrations/2021_09_22_215321_create_product_modifications_tables.php
+++ database/migrations/2021_09_22_215321_create_product_modifications_tables.php
@@ -20,6 +20,7 @@ class CreateProductModificationsTables extends Migration
 //            $table->float('cost_price');
 //            $table->float('margin');
             $table->decimal('selling_price');
+            $table->decimal('stop_list');
             $table->timestamps();
         });
     }
