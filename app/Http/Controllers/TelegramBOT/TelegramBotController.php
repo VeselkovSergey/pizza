0d2f85ff7b0b6521207c7980f9d1b55bbe1cbb33
Sergey <veselok@gmail.com>
2021-12-12 22:35:27 +0300

    bot

--- app/Http/Controllers/TelegramBOT/TelegramBotController.php
+++ app/Http/Controllers/TelegramBOT/TelegramBotController.php
@@ -49,7 +49,20 @@ class TelegramBotController extends Controller
 
                 $report = self::TodayReport();
 
-                $message = '<b>Отчёт:</b>' . PHP_EOL;
+                $message = '<b>Отчёт за сегодня:</b>' . PHP_EOL;
+                $message .= 'Кол-во заказов: ' . $report->countOrder . '(отказов: ' . $report->amountCancelled . ')' . PHP_EOL;
+                $message .= 'Сумма: ' . number_format($report->sum, 2, ',', "'") . ' ₽' . PHP_EOL;
+                $message .= 'Сумма банк: ' . number_format($report->sumBank, 2, ',', "'") . ' ₽' . PHP_EOL;
+                $message .= 'Сумма нал: ' . number_format($report->sumCash, 2, ',', "'") . ' ₽' . PHP_EOL;
+                $message .= 'Средний чек: ' . number_format($report->averageCheck, 2, ',', "'") . ' ₽' . PHP_EOL;
+                $telegram->sendMessage($message);
+                break;
+
+            case '/fullReport':
+
+                $report = self::FullReport();
+
+                $message = '<b>Отчёт за всё время:</b>' . PHP_EOL;
                 $message .= 'Кол-во заказов: ' . $report->countOrder . '(отказов: ' . $report->amountCancelled . ')' . PHP_EOL;
                 $message .= 'Сумма: ' . number_format($report->sum, 2, ',', "'") . ' ₽' . PHP_EOL;
                 $message .= 'Сумма банк: ' . number_format($report->sumBank, 2, ',', "'") . ' ₽' . PHP_EOL;
@@ -68,6 +81,44 @@ class TelegramBotController extends Controller
     public static function TodayReport()
     {
         $orders = OrdersController::OrdersByDate(date('Y-m-d', time()), true);
+        $ordersCount = $orders->count();
+
+        $sum = 0;
+        $sumCash = 0;
+        $sumBank = 0;
+        $amountCancelled = 0;
+
+        foreach ($orders as $order) {
+            $rawData = json_decode($order->all_information_raw_data);
+            $clientInfo = json_decode($order->client_raw_data);
+
+            $sum += $rawData->orderSum;
+
+            if ($clientInfo->typePayment[0] === true) {
+                $sumBank +=  $rawData->orderSum;
+            } else {
+                $sumCash += $rawData->orderSum;
+            }
+
+            if ($order->IsCancelled()) {
+                $amountCancelled++;
+            }
+
+        }
+
+        return (object)[
+            'countOrder' => $ordersCount,
+            'sum' => $sum,
+            'sumBank' => $sumBank,
+            'sumCash' => $sumCash,
+            'averageCheck' => ($sum / $ordersCount),
+            'amountCancelled' => $amountCancelled,
+        ];
+    }
+
+    public static function FullReport()
+    {
+        $orders = OrdersController::AllOrders();
         $ordersCount = $orders->count();
 
         $sum = 0;
