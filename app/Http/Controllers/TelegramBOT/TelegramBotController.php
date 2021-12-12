893bd854f3d68edae11c450c425a4b9b15bcdd97
Sergey <veselok@gmail.com>
2021-12-12 22:07:45 +0300

    bot

--- app/Http/Controllers/TelegramBOT/TelegramBotController.php
+++ app/Http/Controllers/TelegramBOT/TelegramBotController.php
@@ -43,7 +43,7 @@ class TelegramBotController extends Controller
                 $message .= '<i>Показать все команды: /all<i>' . PHP_EOL;
                 $message .= '<i>Получить ID чата /chatId<i>' . PHP_EOL;
 
-                $telegram->sendMessage($message);
+                $telegram->sendMessage('123');
                 break;
         }
     }
