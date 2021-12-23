fad169d990cea5c2c157922ded4cfa0a8035f00d
Sergey Veselkov <veselkov@action-media.ru>
2021-12-23 14:13:54 +0300

    Pusher.php

--- resources/views/arm/test-view/pusher.blade.php
+++ resources/views/arm/test-view/pusher.blade.php
@@ -13,7 +13,7 @@
 
         var channel = pusher.subscribe('my-channel');
         channel.bind('my-event', function(data) {
-            alert(JSON.stringify(data));
+            console.log(data)
         });
     </script>
 </head>
