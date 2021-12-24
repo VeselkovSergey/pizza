d9f0a41033a09b3b79fdbb68e4fa39a358426372
Sergey <veselok@gmail.com>
2021-12-24 22:59:26 +0300

    restyle

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,6 +6,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta name="theme-color" content="#0e0e0e">
 
         @yield('meta')
 
