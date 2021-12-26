b5b2a4267f987f21f5fb2461a74397e4b9cbad1e
Sergey <veselok@gmail.com>
2021-12-26 15:18:12 +0300

    refactor

--- resources/views/arm/management/orders/order.blade.php
+++ resources/views/arm/management/orders/order.blade.php
@@ -133,7 +133,7 @@
                 let courierId = modalWindow.querySelector('.selector-courier input:checked').value;
 
                 Ajax(url, 'post', {orderId: {{$order->id}}, courierId: courierId}).then(() => {
-                    window.close();
+                    location.reload();
                 });
 
             });
