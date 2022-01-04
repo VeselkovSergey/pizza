6865e2cd2293d677f22956ebf64d064d26dc8916
Sergey <veselok@gmail.com>
2022-01-04 16:40:10 +0300

    закрытие свайпом

--- resources/js/script.js
+++ resources/js/script.js
@@ -174,13 +174,13 @@ function ModalWindow(content, closingCallback, flash) {
 
     document.body.append(modalWindowComponentContainer);
 
-    // if (typeof content !== 'string') {
-    //     CloseByScroll(modalWindowComponentContainer, modalWindowContainer, content, () => {
-    //         closingCallback ? closingCallback() : '';
-    //         modalWindowComponentContainer.slowRemove();
-    //         !flash ? documentBody.classList.remove('scroll-off') : '';
-    //     });
-    // }
+    if (typeof content !== 'string') {
+        CloseByScroll(modalWindowComponentContainer, modalWindowContainer, content, () => {
+            closingCallback ? closingCallback() : '';
+            modalWindowComponentContainer.slowRemove();
+            !flash ? documentBody.classList.remove('scroll-off') : '';
+        });
+    }
 
     return modalWindowComponentContainer;
 }
