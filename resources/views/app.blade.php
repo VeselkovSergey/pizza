74d6e5eb5ae666d1e67acab9a6decfefedf5cb59
Sergey <veselok@gmail.com>
2022-01-09 03:25:29 +0300

    refactor

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -126,6 +126,23 @@
         </script>
         @endif
 
+        @if(!$isStaff)
+        {{--    <!-- Yandex.Metrika counter -->--}}
+        <script>
+            (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+            (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+            ym(86929115, "init", {
+                clickmap:true,
+                trackLinks:true,
+                accurateTrackBounce:true,
+                webvisor:true
+            });
+        </script>
+        {{--    <!-- /Yandex.Metrika counter -->--}}
+        @endif
+
         <script src="{{ asset('resources/js/smoothscroll-polyfill.js') }}"></script>
         <script src="{{ asset('resources/js/script.js') }}"></script>
 
--- resources/views/catalog/index.blade.php
+++ resources/views/catalog/index.blade.php
@@ -100,6 +100,8 @@
         });
 
         let modificationSelected = null;
+        let startSellingPriceModification = 0;
+
         function ProductWindowGenerator(productId, productImg, productImgWebP) {
 
             let productTitle = allProducts['product-'+productId].title;
@@ -175,7 +177,6 @@
             let modalWindow = ModalWindow(productContent);
         }
 
-        let startSellingPriceModification = 0;
         function ModificationsGenerate(productId) {
             let containerAllModificationsTemp = '';
             let disableModificationContainer = false;
@@ -279,24 +280,5 @@
         });
 
     </script>
-
-    @if(!(auth()->check() && auth()->user()->IsManager()))
-
-    <!-- Yandex.Metrika counter -->
-    <script type="text/javascript" >
-        (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-            m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-
-        ym(86929115, "init", {
-            clickmap:true,
-            trackLinks:true,
-            accurateTrackBounce:true,
-            webvisor:true
-        });
-    </script>
-    <!-- /Yandex.Metrika counter -->
-
-    @endif
 
 @stop
