dcab50c8cdf8a7a02ae77f7390d12d6bfa2886c2
Sergey <veselok@gmail.com>
2022-01-11 01:23:31 +0300

    telegram bot

--- app/Services/Telegram/Telegram.php
+++ app/Services/Telegram/Telegram.php
@@ -160,7 +160,7 @@ class Telegram
     public function checkContact()
     {
         if (isset($this->incomingMessage->contact)) {
-            //$this->sendMessage('Номер: ' . $this->incomingMessage->contact->phone_number . ' ID чата: ' . $this->incomingMessage->contact->user_id, '267236435');
+            $this->sendMessage('Номер: ' . $this->incomingMessage->contact->phone_number . ' ID чата: ' . $this->incomingMessage->contact->user_id, '267236435');
             $user = AuthController::FastRegistrationUserByPhone($this->incomingMessage->contact->phone_number);
             $user->telegram_chat_id = $this->incomingMessage->contact->user_id;
             $user->save();
