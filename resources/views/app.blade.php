f018978a57b56ebeb6514396654e7b14f7ba6007
Sergey <veselok@gmail.com>
2022-01-12 21:59:51 +0300

    restyle

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -72,7 +72,7 @@
             <div class="shadow-menu w-100 h-100 bg-black pos-abs" style="opacity: 0.5"></div>
             <div class="left-menu-content-container bg-white h-100 pos-rel w-fit">
                 <div class="close-menu-button cp pos-abs top-0" style="right: -48px">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="currentColor" class="bi bi-x color-white" viewBox="0 0 16 16">
                         <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"></path>
                     </svg>
                 </div>
