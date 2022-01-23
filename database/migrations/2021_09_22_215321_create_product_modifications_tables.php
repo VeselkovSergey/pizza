a513c2da052279c5146a0d6b89d557b14b41262f
Sergey <veselok@gmail.com>
2022-01-24 00:56:34 +0300

    модификаторы продукта

--- database/migrations/2021_09_22_215321_create_product_modifications_tables.php
+++ database/migrations/2021_09_22_215321_create_product_modifications_tables.php
@@ -20,7 +20,7 @@ class CreateProductModificationsTables extends Migration
 //            $table->float('cost_price');
 //            $table->float('margin');
             $table->decimal('selling_price');
-            $table->integer('stop_list')->default(0);
+            $table->tinyInteger('stop_list')->default(0);
             $table->tinyInteger('active')->default(0);
             $table->timestamps();
         });
