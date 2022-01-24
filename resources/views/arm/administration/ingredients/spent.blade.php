90d801adf5d3350c41454d85bfd80483714ef083
Sergey <veselok@gmail.com>
2022-01-25 01:17:27 +0300

    +

--- resources/views/arm/administration/ingredients/spent.blade.php
+++ resources/views/arm/administration/ingredients/spent.blade.php
@@ -57,14 +57,14 @@
                     @php($ingredientLastPrice = $ingredient->CurrentPrice())
                     @php($balance = round($ingredient->quantityPurchased - $ingredient->sent, 2))
                     <tr class="hover-color ingredient-container" data-ingredient-id="{{$ingredient->id}}">
-                        <td>#{{$ingredient->id}}</td>
+                        <td class="text-center">#{{$ingredient->id}}</td>
                         <td><input name="title" class="edit-field" readonly type="text" value="{{$ingredient->title}}"></td>
-                        <td>{{$ingredientLastPrice}} ₽</td>
-                        <td>{{round($ingredient->quantityPurchased * $ingredientLastPrice, 2)}} ₽</td>
-                        <td>{{round($ingredient->sent * $ingredientLastPrice, 2)}} ₽</td>
-                        <td>{{$ingredient->quantityPurchased}}</td>
-                        <td>{{$ingredient->sent}}</td>
-                        <td class="@if($balance <= 0) bg-red @endif">{{$balance}}</td>
+                        <td class="text-center">{{$ingredientLastPrice}} ₽</td>
+                        <td class="text-center">{{round($ingredient->quantityPurchased * $ingredientLastPrice, 2)}} ₽</td>
+                        <td class="text-center">{{round($ingredient->sent * $ingredientLastPrice, 2)}} ₽</td>
+                        <td class="text-center">{{$ingredient->quantityPurchased}}</td>
+                        <td class="text-center">{{$ingredient->sent}}</td>
+                        <td class="text-center @if($balance <= 0) bg-red @endif">{{$balance}}</td>
                         <td class="text-center used cp">Где юзается</td>
                     </tr>
                 @endforeach
