be2328d1f9d8cd35f7572b5a9902b9c8324a8a29
Sergey <veselok@gmail.com>
2022-01-25 01:50:31 +0300

    +

--- resources/views/arm/supply/index.blade.php
+++ resources/views/arm/supply/index.blade.php
@@ -11,28 +11,28 @@
             <table class="w-100 border table-sort">
                 <thead>
                 <tr>
-                    <th>#</th>
+                    <th class="w-0">#</th>
                     <th>Поставщик</th>
                     <th>Дата поставки</th>
                     <th>Тип оплаты</th>
                     <th>Сумма</th>
                     <th>Кол-во позиций</th>
                     <th>Кто создал</th>
-                    <th></th>
+                    <th class="w-0"></th>
                 </tr>
                 </thead>
                 <tbody>
                 @foreach($supplies as $supply)
                     <?php /** @var \App\Models\Supply $supply */ ?>
                     <tr>
-                        <td>{{$supply->id}}</td>
-                        <td>{{$supply->Supplier->title}}</td>
+                        <td class="text-center">{{$supply->id}}</td>
+                        <td class="text-center">{{$supply->Supplier->title}}</td>
                         <td>{{$supply->supply_date}}</td>
-                        <td>{{$supply->PaymentType()}}</td>
-                        <td>{{$supply->SupplySum()}}</td>
-                        <td>{{$supply->Ingredients->count()}}</td>
-                        <td>{{$supply->Creator->name}}</td>
-                        <td><a href="{{route('supply-detail-page', $supply->id)}}">Подробнее</a></td>
+                        <td class="text-center">{{$supply->PaymentType()}}</td>
+                        <td class="text-center">{{$supply->SupplySum()}}</td>
+                        <td class="text-center">{{$supply->Ingredients->count()}}</td>
+                        <td class="text-center">{{$supply->Creator->name}}</td>
+                        <td class="text-center"><a href="{{route('supply-detail-page', $supply->id)}}">Подробнее</a></td>
                     </tr>
                 @endforeach
                 </tbody>
