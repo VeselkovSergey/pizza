d2603fd9f9c4e5a74d7a3d752b4224b7c61d10fa
Sergey <veselok@gmail.com>
2022-01-25 01:08:58 +0300

    +

--- resources/views/arm/administration/ingredients/spent.blade.php
+++ resources/views/arm/administration/ingredients/spent.blade.php
@@ -43,11 +43,11 @@
                 <tr>
                     <th class="w-0">ID</th>
                     <th>Наименование</th>
-                    <th class="w-0">Сумма закупки</th>
                     <th class="w-0">Актуальная цена за кг/шт</th>
+                    <th class="w-0">Сумма закупки</th>
+                    <th class="w-0">Потрачено в деньгах</th>
                     <th class="w-0">Кол-во закупки</th>
                     <th class="w-0">Потрачено в единицах</th>
-                    <th class="w-0">Потрачено в деньгах</th>
                     <th class="w-0">Остаток</th>
                     <th class="w-0"></th>
                 </tr>
@@ -59,11 +59,11 @@
                     <tr class="hover-color ingredient-container" data-ingredient-id="{{$ingredient->id}}">
                         <td>#{{$ingredient->id}}</td>
                         <td><input name="title" class="edit-field" readonly type="text" value="{{$ingredient->title}}"></td>
-                        <td>{{$ingredient->quantityPurchased * $ingredientLastPrice}}</td>
                         <td>{{$ingredientLastPrice}} ₽</td>
+                        <td>{{$ingredient->quantityPurchased * $ingredientLastPrice}}</td>
+                        <td>{{$ingredient->sent * $ingredientLastPrice}}</td>
                         <td>{{$ingredient->quantityPurchased}}</td>
                         <td>{{$ingredient->sent}}</td>
-                        <td>{{$ingredient->sent * $ingredientLastPrice}}</td>
                         <td class="@if($balance <= 0) bg-red @endif">{{$balance}}</td>
                         <td class="text-center used cp">Где юзается</td>
                     </tr>
