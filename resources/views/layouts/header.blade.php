b4800beee1619e6ebf0d9b4361c2341562f63d68
Sergey <veselok@gmail.com>
2022-01-29 01:19:48 +0300

    часы работы в шапке

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -14,7 +14,12 @@
     BROпицца
 </div>
 
-<div class="button-menu flex-center cp mr-10 @if(auth()->check() && auth()->user()->IsManager()) ml-a @endif">
+<div class="flex-column-center mr-10 @if(auth()->check() && auth()->user()->IsManager()) ml-a @endif">
+    <div>10:00</div>
+    <div>22:30</div>
+</div>
+
+<div class="button-menu flex-center cp mr-10">
     <div class="color-white flex-center border-radius-5 px-10 border-orange">
         <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
             <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
