17ad7855ca6854e31475d135cb9e3a4e4a291bce
Sergey <veselok@gmail.com>
2022-01-30 18:27:32 +0300

    restyle

--- resources/views/arm/administration/orders/index.blade.php
+++ resources/views/arm/administration/orders/index.blade.php
@@ -6,6 +6,7 @@
         .order:hover {
             background-color: wheat;
         }
+
         @media screen and (max-width: 540px) {
             .start-date,
             .end-date {
@@ -46,38 +47,43 @@
     @php($ordersByCouriers = [])
 
     <div class="flex-column">
+
         <div style="order: 2;">
-            <button class="orange-button show-all-columns">Показать все колонки</button>
+            <div class="toggle-button cp" data-toogle="toggle-buttons-container">Показать фильтр колонок</div>
+            <div>
+                <div class="toggle-buttons-container"></div>
+            </div>
         </div>
+
         <div style="order: 3;">
             <table class="w-100 border table-sort" style="width: max-content;">
                 <thead>
                 <tr>
-                    <th data-title-column-id="1" class="change-visible-column w-0">ID</th>
-                    <th data-title-column-id="2" class="change-visible-column ">Статус</th>
-                    <th data-title-column-id="3" class="change-visible-column ">Дата создания</th>
-                    <th data-title-column-id="4" class="change-visible-column ">Дата изменения последнего статуса</th>
-                    <th data-title-column-id="5" class="change-visible-column w-0">Потрачено времени всего</th>
-                    <th data-title-column-id="6" class="change-visible-column w-0">Передан на кухню -> Доставлен</th>
-                    <th data-title-column-id="7" class="change-visible-column w-0">Менеджер зашел в заказ</th>
-                    <th data-title-column-id="8" class="change-visible-column w-0">Менеджер передал на кухню</th>
-                    <th data-title-column-id="9" class="change-visible-column w-0">Кухня приготовила</th>
-                    <th data-title-column-id="10" class="change-visible-column w-0">Передан в доставку</th>
-                    <th data-title-column-id="11" class="change-visible-column w-0">Доставлен</th>
-                    <th data-title-column-id="12" class="change-visible-column w-0">Деньги в кассе</th>
-                    <th data-title-column-id="13" class="change-visible-column w-0">Кол-во позиций</th>
-                    <th data-title-column-id="14" class="change-visible-column w-0">Курьер</th>
-                    <th data-title-column-id="15" class="change-visible-column w-0">Номер заказавшего</th>
-                    <th data-title-column-id="16" class="change-visible-column ">Комментарий</th>
-                    <th data-title-column-id="17" class="change-visible-column w-0">Тип заказа</th>
-                    <th data-title-column-id="18" class="change-visible-column w-0">Сумма</th>
-                    <th data-title-column-id="19" class="change-visible-column w-0"></th>
-                    <th data-title-column-id="20" class="change-visible-column w-0">Себестоимость заказа</th>
+                    <th data-title-column-id="1" class="table-columns w-0">ID</th>
+                    <th data-title-column-id="2" class="table-columns ">Статус</th>
+                    <th data-title-column-id="3" class="table-columns ">Дата создания</th>
+                    <th data-title-column-id="4" class="table-columns ">Дата изменения последнего статуса</th>
+                    <th data-title-column-id="5" class="table-columns w-0">Потрачено времени всего</th>
+                    <th data-title-column-id="6" class="table-columns w-0">Передан на кухню -> Доставлен</th>
+                    <th data-title-column-id="7" class="table-columns w-0">Менеджер зашел в заказ</th>
+                    <th data-title-column-id="8" class="table-columns w-0">Менеджер передал на кухню</th>
+                    <th data-title-column-id="9" class="table-columns w-0">Кухня приготовила</th>
+                    <th data-title-column-id="10" class="table-columns w-0">Передан в доставку</th>
+                    <th data-title-column-id="11" class="table-columns w-0">Доставлен</th>
+                    <th data-title-column-id="12" class="table-columns w-0">Деньги в кассе</th>
+                    <th data-title-column-id="13" class="table-columns w-0">Кол-во позиций</th>
+                    <th data-title-column-id="14" class="table-columns w-0">Курьер</th>
+                    <th data-title-column-id="15" class="table-columns w-0">Номер заказавшего</th>
+                    <th data-title-column-id="16" class="table-columns ">Комментарий</th>
+                    <th data-title-column-id="17" class="table-columns w-0">Тип заказа</th>
+                    <th data-title-column-id="18" class="table-columns w-0">Сумма</th>
+                    <th data-title-column-id="19" class="table-columns w-0">Подробнее</th>
+                    <th data-title-column-id="20" class="table-columns w-0">Себестоимость заказа</th>
                 </tr>
                 </thead>
                 <tbody>
                 @foreach($orders as $order)
-                    <?php /** @var \App\Models\Orders $order  */ ?>
+                    <?php /** @var \App\Models\Orders $order */ ?>
 
                     @php($clientInfo = json_decode($order->client_raw_data))
                     @php($productsModificationsInOrder = $order->ProductsModifications)
@@ -134,12 +140,17 @@
                     @endif
 
                     <tr class="order">
-                        <td data-column-id="1"><a target="_blank" href="{{route('manager-arm-order-page', $order->id)}}">{{$order->id}}</a></td>
-                        <td data-column-id="2" class="order-status-{{$order->status_id}}">{{\App\Models\Orders::STATUS[$order->status_id]}}</td>
+                        <td data-column-id="1"><a target="_blank"
+                                                  href="{{route('manager-arm-order-page', $order->id)}}">{{$order->id}}</a>
+                        </td>
+                        <td data-column-id="2"
+                            class="order-status-{{$order->status_id}}">{{\App\Models\Orders::STATUS[$order->status_id]}}</td>
                         <td data-column-id="3">{{$order->created_at}}</td>
                         <td data-column-id="4">{{$order->LatestStatus->updated_at}}</td>
-                        <td data-column-id="5" class="text-center" @if($longTime) style="background-color: #e37e7e;" @endif>{{date_diff($order->created_at, $order->LatestStatus->updated_at)->format('%H:%I:%S')}}</td>
-                        <td data-column-id="6" class="text-center" @if($longTimeDelivered) style="background-color: #e37e7e;" @endif>{{\App\Models\Orders::TimeBetweenStatuses($order->id, \App\Models\Orders::STATUS_TEXT['kitchen'], \App\Models\Orders::STATUS_TEXT['delivered'])}}</td>
+                        <td data-column-id="5" class="text-center"
+                            @if($longTime) style="background-color: #e37e7e;" @endif>{{date_diff($order->created_at, $order->LatestStatus->updated_at)->format('%H:%I:%S')}}</td>
+                        <td data-column-id="6" class="text-center"
+                            @if($longTimeDelivered) style="background-color: #e37e7e;" @endif>{{\App\Models\Orders::TimeBetweenStatuses($order->id, \App\Models\Orders::STATUS_TEXT['kitchen'], \App\Models\Orders::STATUS_TEXT['delivered'])}}</td>
                         <td data-column-id="7" class="text-center">{{$order->TimeManagerProcesses()}}</td>
                         <td data-column-id="8" class="text-center">{{$order->TimeTransferOnKitchen()}}</td>
                         <td data-column-id="9" class="text-center">{{$order->TimeCooked()}}</td>
@@ -147,7 +158,8 @@
                         <td data-column-id="11" class="text-center">{{$order->TimeDelivered()}}</td>
                         <td data-column-id="12" class="text-center">{{$order->TimeCompleted()}}</td>
                         <td data-column-id="13" class="text-center">{{$productsModificationsInOrder->count()}}</td>
-                        <td data-column-id="14">{{$order->courier_id}}&nbsp;{{isset($order->Courier) ? '('.$order->Courier->name.')' : ''}}</td>
+                        <td data-column-id="14">{{$order->courier_id}}
+                            &nbsp;{{isset($order->Courier) ? '('.$order->Courier->name.')' : ''}}</td>
                         <td data-column-id="15">{{$order->User->phone}}</td>
                         <td data-column-id="16">{{$clientInfo->clientComment}}</td>
                         <td data-column-id="17" class="text-center">{{$orderCreator}}</td>
@@ -165,9 +177,9 @@
                                         @foreach($modificationIngredients as $ingredient)
                                             <?php
                                             /** @var \App\Models\ProductModificationsIngredients $ingredient */
-                                                $sumIngredient = $ingredient->ingredient_amount * $ingredient->Ingredient->CurrentPrice();
+                                            $sumIngredient = $ingredient->ingredient_amount * $ingredient->Ingredient->CurrentPrice();
                                             ?>
-                                                @php($costPrice += $sumIngredient)
+                                            @php($costPrice += $sumIngredient)
                                         @endforeach
                                         @php($orderCost += $costPrice * $productModificationInOrder->product_modification_amount)
 
@@ -186,7 +198,7 @@
                         </td>
                         <td data-column-id="20">{{$orderCost}}</td>
                     </tr>
-                    @endforeach
+                @endforeach
                 </tbody>
             </table>
         </div>
@@ -195,10 +207,16 @@
             <div class="mb-10">Итого: {{$sum}} (Наличные: {{$sumCash}} / Банк: {{$sumBank}})</div>
             <div class="mb-10">Себестоимость: {{$sumCost}}</div>
             <div class="mb-10">Поставки: {{$supplySum}}</div>
-            <div class="mb-10">Кол-во заказов: {{$orders->count()}} (Сайт: {{$ordersCreatorWeb}} / Менеджер {{$ordersCreatorManager}} / Собственник {{$ordersCreatorAdmin}} / Отказ {{$amountOrdersCancelled}})</div>
-            <div class="mb-10">Средний чек: {{($orders->count() - $amountOrdersCancelled) !== 0 ? ($sum / ($orders->count() - $amountOrdersCancelled)) : 0}}</div>
+            <div class="mb-10">Кол-во заказов: {{$orders->count()}} (Сайт: {{$ordersCreatorWeb}} /
+                Менеджер {{$ordersCreatorManager}} / Собственник {{$ordersCreatorAdmin}} /
+                Отказ {{$amountOrdersCancelled}})
+            </div>
+            <div class="mb-10">Средний
+                чек: {{($orders->count() - $amountOrdersCancelled) !== 0 ? ($sum / ($orders->count() - $amountOrdersCancelled)) : 0}}</div>
             <div class="mb-10">
-                <div class="toggle-button cp" data-toogle="amount-orders-in-days-container">Кол-во заказов в день (нал/банк/всего) (нажать. раскроется.)</div>
+                <div class="toggle-button cp" data-toogle="amount-orders-in-days-container">Кол-во заказов в день
+                    (нал/банк/всего) (нажать. раскроется.)
+                </div>
                 <div class="amount-orders-in-days-container">
                     @foreach($amountOrdersInDays as $date => $amountOrdersInDay)
                         @php($amountOrdersInDayCash = $amountOrdersInDay['cash'] ?? 0)
@@ -207,8 +225,11 @@
                         @php($amountOrdersInDayDateBank = $sumOrdersInDays[$date]['bank'] ?? 0)
                         <div class="flex">
                             <div class="mr-10" style="width: 170px;">Дата: {{date('Y-m-d', strtotime($date))}}</div>
-                            <div class="mr-10" style="width: 170px;">Кол-во: {{$amountOrdersInDayCash}}/{{$amountOrdersInDayBank}}/{{$amountOrdersInDayCash + $amountOrdersInDayBank}}</div>
-                            <div class="mr-10">Cумма: {{$amountOrdersInDayDateCash}} ₽ /{{$amountOrdersInDayDateBank}} ₽ /{{$amountOrdersInDayDateCash + $amountOrdersInDayDateBank}} ₽</div>
+                            <div class="mr-10" style="width: 170px;">Кол-во: {{$amountOrdersInDayCash}}
+                                /{{$amountOrdersInDayBank}}/{{$amountOrdersInDayCash + $amountOrdersInDayBank}}</div>
+                            <div class="mr-10">Cумма: {{$amountOrdersInDayDateCash}} ₽ /{{$amountOrdersInDayDateBank}} ₽
+                                /{{$amountOrdersInDayDateCash + $amountOrdersInDayDateBank}} ₽
+                            </div>
                         </div>
                     @endforeach
                 </div>
@@ -298,7 +319,7 @@
         // }
 
         // Выводим меток
-        for(let i = 0; i <= 13; i++) {
+        for (let i = 0; i <= 13; i++) {
             ctx.fillText(i + 10, ((i * 100)), 510);
         }
 
@@ -307,28 +328,31 @@
         // Назначаем зелёный цвет для графика
         ctx.fillStyle = "green";
         // Цикл для от рисовки графиков
-        for(let i = 0; i < 23; i++) {
-            let dp = data[i+10];
-            ctx.fillRect(i*100, 500-dp*5 , 5, dp*5);
-            ctx.fillText(data[i+10], ((i * 100)), 490-dp*5);
+        for (let i = 0; i < 23; i++) {
+            let dp = data[i + 10];
+            ctx.fillRect(i * 100, 500 - dp * 5, 5, dp * 5);
+            ctx.fillText(data[i + 10], ((i * 100)), 490 - dp * 5);
         }
 
-        document.body.querySelector('.show-all-columns').addEventListener('click', () => {
-            document.body.querySelectorAll('.change-visible-column').forEach((columnTitle) => {
-                let columnId = columnTitle.dataset.titleColumnId;
-                columnTitle.show();
-                document.body.querySelectorAll('td[data-column-id="'+columnId+'"]').forEach((column) => {
-                    column.show();
-                });
-            });
-        });
+        const toggleButtonContainer = document.body.querySelector('.toggle-buttons-container');
+        document.body.querySelectorAll('.table-columns').forEach((columnTitle) => {
 
-        document.body.querySelectorAll('.change-visible-column').forEach((columnTitle) => {
             let columnId = columnTitle.dataset.titleColumnId;
-            columnTitle.addEventListener('dblclick', () => {
-                columnTitle.hide();
-                document.body.querySelectorAll('td[data-column-id="'+columnId+'"]').forEach((column) => {
-                    column.hide();
+
+            let content = '<label class="custom-checkbox-label" for="column-title-id-'+columnId+'">' +
+                                '<input class="change-visible-column" data-toggler-column-id="" type="checkbox" id="column-title-id-'+columnId+'" checked />' +
+                                '<div class="custom-checkbox-slider round"></div>' +
+                            '</label>' +
+                            '<span>'+columnTitle.innerHTML+'</span>';
+
+            let toggleButton = CreateElement('div', {content: content});
+
+            toggleButtonContainer.append(toggleButton);
+
+            toggleButton.addEventListener('change', () => {
+                document.body.querySelector('th[data-title-column-id="' + columnId + '"]').showToggle();
+                document.body.querySelectorAll('td[data-column-id="' + columnId + '"]').forEach((column) => {
+                    column.showToggle();
                 });
             });
         });
