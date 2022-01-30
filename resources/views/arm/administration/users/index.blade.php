3214cebd01f9f086716c36f3a2fd39cca0e86a2f
Sergey <veselok@gmail.com>
2022-01-30 15:37:20 +0300

    restyle

--- resources/views/arm/administration/users/index.blade.php
+++ resources/views/arm/administration/users/index.blade.php
@@ -22,26 +22,26 @@
             <table class="w-100 border table-sort">
                 <thead>
                 <tr>
-                    <th>ID</th>
+                    <th class="w-0">ID</th>
                     <th>Имя</th>
-                    <th>Номер</th>
-                    <th>Роль</th>
-                    <th>Работает</th>
-                    <th>ID чата в телеграм</th>
-                    <th>Кол-во заказов</th>
-                    <th></th>
-                    <th></th>
+                    <th class="w-0">Номер</th>
+                    <th class="w-0">Роль</th>
+                    <th class="w-0">Работает</th>
+                    <th class="w-0">ID чата в телеграм</th>
+                    <th class="w-0">Кол-во заказов</th>
+                    <th class="w-0"></th>
+                    <th class="w-0"></th>
                 </tr>
                 </thead>
                 <tbody>
                 @foreach($users as $user)
                     <?php /** @var \App\Models\User $user */?>
                     <tr class="user-info-container hover-color" data-user-id="{{$user->id}}">
-                        <td>{{$user->id}}</td>
+                        <td class="text-center">{{$user->id}}</td>
                         <td><input name="name" class="edit-field" readonly type="text" value="{{$user->name}}"></td>
-                        <td>{{$user->phone}}</td>
-                        <td><input name="role_id" class="edit-field" readonly type="text" value="{{$user->role_id}}"></td>
-                        <td>
+                        <td class="text-center">{{$user->phone}}</td>
+                        <td class="text-center"><input name="role_id" class="edit-field" readonly type="text" value="{{$user->role_id}}"></td>
+                        <td class="text-center">
                             <div class="flex-center">
                                 <label class="custom-checkbox-label" for="is_employee-{{$user->id}}">
                                     <input class="edit-field" type="checkbox" id="is_employee-{{$user->id}}" name="is_employee" @if($user->is_employee) checked @endif/>
@@ -49,10 +49,10 @@
                                 </label>
                             </div>
                         </td>
-                        <td><input name="telegram_chat_id" class="edit-field" readonly type="text" value="{{$user->telegram_chat_id}}"></td>
-                        <td>{{$user->Orders->count()}}</td>
-                        <td><a href="{{route('administrator-arm-user-orders-page', $user->id)}}">к заказам</a></td>
-                        <td><a href="{{route('all-sessions-page')}}">к сессиям</a></td>
+                        <td class="text-center"><input name="telegram_chat_id" class="edit-field" readonly type="text" value="{{$user->telegram_chat_id}}"></td>
+                        <td class="text-center">{{$user->Orders->count()}}</td>
+                        <td class="text-center"><a href="{{route('administrator-arm-user-orders-page', $user->id)}}">к заказам</a></td>
+                        <td class="text-center"><a href="{{route('all-sessions-page')}}">к сессиям</a></td>
 
                     </tr>
                 @endforeach
