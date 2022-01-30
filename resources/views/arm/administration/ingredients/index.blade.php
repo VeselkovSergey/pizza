42155d50fc8acea24b942e905309179e82015ef3
Sergey <veselok@gmail.com>
2022-01-30 14:51:42 +0300

    restyle

--- resources/views/arm/administration/ingredients/index.blade.php
+++ resources/views/arm/administration/ingredients/index.blade.php
@@ -11,22 +11,22 @@
             <table class="w-100 border table-sort">
                 <thead>
                 <tr>
-                    <th>ID</th>
+                    <th class="w-0">ID</th>
                     <th>Наименование</th>
-                    <th>Актуальная цена за кг/шт</th>
-                    <th>Кол-во в последней поставке</th>
-                    <th>Дата поставки</th>
+                    <th class="w-0">Актуальная цена за кг/шт</th>
+                    <th class="w-0">Кол-во в последней поставке</th>
+                    <th class="w-10">Дата поставки</th>
                 </tr>
                 </thead>
                 <tbody>
                 @foreach($ingredients as $ingredient)
                     @php($ingredientLastSupply = $ingredient->LastSupply())
-                    <tr>
-                        <td>#{{$ingredient->id}}</td>
+                    <tr class="hover-color">
+                        <td class="text-center">#{{$ingredient->id}}</td>
                         <td>{{$ingredient->title}}</td>
-                        <td>{{$ingredientLastSupply->price_ingredient}} ₽</td>
-                        <td>{{$ingredientLastSupply->amount_ingredient}}</td>
-                        <td>{{$ingredientLastSupply->supply_date}}</td>
+                        <td class="text-center">{{$ingredientLastSupply->price_ingredient}} ₽</td>
+                        <td class="text-center">{{$ingredientLastSupply->amount_ingredient}}</td>
+                        <td class="text-center">{{$ingredientLastSupply->supply_date}}</td>
                     </tr>
                 @endforeach
                 </tbody>
