2a81b9a32d3bff9707713bf93004dee01f14a122
Sergey <veselok@gmail.com>
2022-02-14 02:14:01 +0300

    дата поставки в детях поставки

--- resources/views/arm/supply/detail.blade.php
+++ resources/views/arm/supply/detail.blade.php
@@ -75,7 +75,7 @@
             </div>
 
             <h3 style="order: 1">Сумма: {{round($sum, 2)}} ₽</h3>
-            <div style="order: 1">{{$supply->created_at}}</div>
+            <div style="order: 1">{{$supply->supply_date}}</div>
             <div style="order: 1" class="mb-10">{{$supply->Creator->name}}</div>
         </div>
 
