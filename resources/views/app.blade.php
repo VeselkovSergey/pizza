1bd9c8550b6ea39d562a5c3df7b743938c510f44
Sergey <veselok@gmail.com>
2022-02-13 21:13:26 +0300

    отключил масштабирование на айфоне

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -26,8 +26,7 @@
     <head>
 
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, viewport-fit=cover, shrink-to-fit=no" />
-        <meta name="HandheldFriendly" content="true">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta name="theme-color" content="#0e0e0e">
 
