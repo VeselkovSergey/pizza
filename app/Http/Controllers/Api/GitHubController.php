d2703eb6ef577211bf6aeff3f66a4e6bff9a4382
VeselkovSergey <66443805+VeselkovSergey@users.noreply.github.com>
2022-10-08 11:30:10 +0300

    Update GitHubController.php

--- app/Http/Controllers/Api/GitHubController.php
+++ app/Http/Controllers/Api/GitHubController.php
@@ -10,10 +10,12 @@ class GitHubController extends ApiController
     public function Push(Request $request)
     {
         // sudo -u www-data ssh-keygen - генерим ssh ключи под www-data
-        // chmod 600 /var/www/.ssh/ida_rsa - для работы git pull под www-data
+        // sudo chmod 600 /var/www/.ssh/id_rsa.pub - для работы git pull под www-data
+        echo '<pre>' . PHP_EOL;
         echo 'git pull start' . PHP_EOL;
         echo shell_exec('git pull');
         echo 'git pull complete' . PHP_EOL;
+        echo '</pre>' . PHP_EOL;
     }
 
     public function test(Request $request)
